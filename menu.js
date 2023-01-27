eb2f55782dc5aa172712e16473d4c5660726481f
sumit7557 <117259880+sumit7557@users.noreply.github.com>
2023-01-27 04:49:00 -0800

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,39 @@
+document.getElementById("icon-click").onclick = function(){
+    // alert()
+    var menu = document.getElementById('vertical-menu');
+    var record = menu.style.display;
+    // alert(record)
+    if(record == "" || record=="none"){
+        menu.style.display = "block";
+    }
+    else{
+        menu.style.display = "none";
+    }
+}
+// slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
